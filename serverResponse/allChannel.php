5b4acb17aab969ab02ddd51aa6c00a4cc0ef137d
zxj5470 <zxj5470@foxmail.com>
2017-06-04 11:38:43 +0800

    ok

---
+++ serverResponse/allChannel.php
@@ -0,0 +1,37 @@
+<?php
+header("Content-Type: application/json;charset=utf-8");
+header("Access-Control-Allow-Origin: *");
+
+function json_encode_ex($value){
+    if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+        $str = json_encode($value);
+        $str = preg_replace_callback(
+            "#\\\u([0-9a-f]{4})#i",
+            function ($matchs) {
+                return iconv('UCS-2BE', 'UTF-8', pack('H4', $matchs[1]));
+            },
+            $str
+        );
+        return $str;
+    } else {
+        return json_encode($value, JSON_UNESCAPED_UNICODE);
+    }
+}
+
+$servername = "192.168.104.3";
+$username = "root";
+$password = "zxj5470@A";
+$dbname = "Hackday";$conn = new mysqli($servername, $username, $password,$dbname);
+$conn->query("set names utf8");
+
+$sql = "SELECT UNIX_TIMESTAMP(ID) ID,SName,Status,Channel FROM CurChannel ORDER BY Status ASC;";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $results[]=$row;
+  }
+echo json_encode_ex($results);
+}
+mysql_close($conn);
+?>
+
---
+++ serverResponse/change2.php
@@ -0,0 +1,44 @@
+<?php
+header("Content-Type: application/json;charset=utf-8");
+header("Access-Control-Allow-Origin: *");
+
+function json_encode_ex($value){
+    if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+        $str = json_encode($value);
+        $str = preg_replace_callback(
+            "#\\\u([0-9a-f]{4})#i",
+            function ($matchs) {
+                return iconv('UCS-2BE', 'UTF-8', pack('H4', $matchs[1]));
+            },
+            $str
+        );
+        return $str;
+    } else {
+        return json_encode($value, JSON_UNESCAPED_UNICODE);
+    }
+}
+
+$servername = "192.168.104.3";
+$username = "root";
+$password = "zxj5470@A";
+$dbname = "Hackday";$conn = new mysqli($servername, $username, $password,$dbname);
+$conn->query("set names utf8");
+$thisID=$_GET['ID'];
+$thisS=$_GET['S'];
+
+$sql='select * from thisChannel';
+$result=$conn->query($sql);
+$row = $result->fetch_assoc();
+$C=$row['Value'];
+
+if($thisID==null){
+  echo 'params valid!';
+}else{
+if($thisS==null)$thisS=1;
+$sql = "UPDATE CurChannel Set Status=".$thisS."  WHERE UNIX_TIMESTAMP(ID)='".$thisID."' and Channel='".$C."'";
+$result = $conn->query($sql);
+echo 'update~';
+}
+mysql_close($conn);
+?>
+
